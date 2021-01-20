439952af8b9ba2b723bb67febe3a086fd2a637cd
Naveen <rajnaveen677@gmail.com>
2021-01-20 20:04:46 +0530

    commit46

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,34 @@
+// Opening the popup and triggering autoplay
 function OpenOverlay(id) {
   let OpenVideo = document.getElementById(id);
 
   if ((OpenVideo.style.display = "none")) {
     OpenVideo.style.display = "block";
   }
+
+  // triggering the iframe to autoplay
   var player = new Vimeo.Player(OpenVideo);
   player.play();
 }
+
+// Closing Popup
+jQuery(document).ready(function () {
+  jQuery(
+    ".wrapper .main-content .Overlay .Job1-overlay .close-button button"
+  ).on("click", function () {
+    1;
+    var iframeId = $(this).val();
+    var $frame = jQuery(
+      ".wrapper .main-content .Overlay .Job1-overlay iframe#" + iframeId
+    );
+    // saves the current iframe source
+    var vidsrc = $frame.attr("src");
+
+    // sets the source to nothing, stopping the video
+    $frame.attr("src", "");
+
+    // sets it back to the correct link so that it reloads immediately on the next window open
+    $frame.attr("src", vidsrc);
+    jQuery(".wrapper .main-content .Overlay .Job1-overlay").hide();
+  });
+});
